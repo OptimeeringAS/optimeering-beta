0ca923164baf586b101e632fb7790022cf520377
OptimeeringBigya <bigya.pradhan@optimeering.com>
2024-11-01 13:15:31 +0100

    Regenerate client

--- optimeering_beta/configuration.py
+++ optimeering_beta/configuration.py
@@ -60,9 +60,10 @@ class Configuration:
         self,
         host: Optional[str] = "https://beta.optimeering.com",
         api_auth_url: Optional[str] = None,
-        api_key: Optional[str] = None,
-        retries=None,
         *,
+        api_key: Optional[str] = None,
+        token: Optional[str] = None,
+        retries=None,
         debug: Optional[bool] = None,
     ) -> None:
         """Constructor"""
@@ -70,7 +71,14 @@ class Configuration:
         self.api_auth_url = (
             f"api://{urlparse(self._base_path).netloc}/.default" if api_auth_url is None else api_auth_url
         )
+
+        # Check auth conflict
+        auth_parameters = (api_key, token)
+        if sum((auth_param is not None) for auth_param in auth_parameters) > 1:
+            raise Exception("Only a single mode of authorization can be used.")
+
         self.api_key = api_key
+        self.token = token
 
         self.logger = {}
         """Logging Settings
