2f90df1af8d154438ef38a428a523b69c2fc062b
Scott Melhop <scott.melhop@optimeering.com>
2025-03-17 15:33:13 +0100

    Make retrieve versioned very simple

--- examples/retrieve_versioned.py
+++ examples/retrieve_versioned.py
@@ -3,15 +3,8 @@ from optimeering_beta import OptimeeringClient
 client = OptimeeringClient()
 
 versioned_series = client.predictions_api.list_version(product=["Imbalance"], area=["NO1"], statistic=["Point"], unit_type=["Price_Spread"])
-
-# Example of how to find the last two versions for this series
-versions = [s.version for s in versioned_series.items]
-versions.sort(key=lambda s: list(map(int, s.split('.'))))
-last_two_versions = versions[-2:]
-
-
-filtered_versioned_series = versioned_series.filter(version=last_two_versions)
-data = versioned_series.retrieve_versioned(start="-P1M", include_simulated=True)
+filtered_versions = versioned_series.filter(version=["1.2.1"])
+data = filtered_versions.retrieve_versioned(start="-P1M", include_simulated=True)
 
 df = data.to_pandas(unpack_value_method="new_rows")
 
